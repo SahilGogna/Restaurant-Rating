ff468e96793dd101bd9787f6590c44b24b6dabd7
Sahil Gogna <gogna.sahil95@gmail.com>
2019-02-21 12:55:37 -0500

    UI for 2 activities

---
+++ RestaurantRatingApp/app/src/main/java/com/example/sahilgogna/restaurantratingapp/ClientDashboard.java
@@ -0,0 +1,13 @@
+package com.example.sahilgogna.restaurantratingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClientDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client_dashboard);
+    }
+}
